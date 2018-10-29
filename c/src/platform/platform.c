9db7d01b474b63cec38eb6cc0e3704139d905ba4
Stef D'haeseleer <r0583050@pc-klas5-7.esat.kuleuven.be>
2018-10-29 14:52:16 +0100

    Small change in platform file

--- c/src/platform/platform.c
+++ c/src/platform/platform.c
@@ -43,39 +43,6 @@
 /*#include "ps7_init.h"*/
 /*#include "psu_init.h"*/
 
-#ifdef STDOUT_IS_16550
- #include "xuartns550_l.h"
-
- #define UART_BAUD 9600
-#endif
-
-void enable_caches() {
-#ifdef __PPC__
-    Xil_ICacheEnableRegion(CACHEABLE_REGION_MASK);
-    Xil_DCacheEnableRegion(CACHEABLE_REGION_MASK);
-#elif __MICROBLAZE__
-#ifdef XPAR_MICROBLAZE_USE_ICACHE
-    Xil_ICacheEnable();
-#endif
-#ifdef XPAR_MICROBLAZE_USE_DCACHE
-    Xil_DCacheEnable();
-#endif
-#endif
-}
-
-void disable_caches() {
-    Xil_DCacheDisable();
-    Xil_ICacheDisable();
-}
-
-void init_uart() {
-#ifdef STDOUT_IS_16550
-    XUartNs550_SetBaud(STDOUT_BASEADDR, XPAR_XUARTNS550_CLOCK_HZ, UART_BAUD);
-    XUartNs550_SetLineControlReg(STDOUT_BASEADDR, XUN_LCR_8_DATA_BITS);
-#endif
-    /* Bootrom/BSP configures PS7/PSU UART to 115200 bps */
-}
-
 void init_platform() {
     /*
      * If you want to run this example outside of SDK,
