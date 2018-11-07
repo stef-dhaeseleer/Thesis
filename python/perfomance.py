3eb1457e363d21a9f84b448e39c1876ce3b76952
Stef D'haeseleer <r0583050@pc-klas2-8.esat.kuleuven.be>
2018-11-07 09:56:03 +0100

    New python file for performance calculations

---
+++ python/perfomance.py
@@ -0,0 +1,26 @@
+import math
+
+def main():
+    
+	clock_freq = 107	# Clock frequency in MHz
+	nb_blocks = 36		# Number of DES calculation blocks
+
+	seconds_per_day = 24*60*60
+	
+	encrypt_per_day = nb_blocks * clock_freq * 10**6 * seconds_per_day
+	two_exp = math.log(encrypt_per_day, 2)
+
+	print ()
+	print ("################################################################################")
+	print ()
+
+	print ("Clock frequency     : " + str(clock_freq))
+	print ("DES blocks          : " + str(nb_blocks))
+	print ("Encryptions per day : " + str(encrypt_per_day))
+	print ("Encryptions 2 exp   : " + str(two_exp))
+
+	print ()
+
+if __name__ == '__main__':
+    main()
+
