d8ca4a1a58b10175a85e8f38deebb02cbbe01cdf
Stef D'haeseleer <r0583050@pc-klas2-16.esat.kuleuven.be>
2018-11-27 13:53:22 +0100

    changed performance calculation file

--- python/perfomance.py
+++ python/perfomance.py
@@ -2,25 +2,38 @@ import math
 
 def main():
     
-	clock_freq = 214	# Clock frequency in MHz
+	clock_freq = 200	# Clock frequency in MHz
 	nb_blocks = 32		# Number of DES calculation blocks
+
+	N = 24
 
 	seconds_per_day = 24*60*60
 	
 	encrypt_per_day = nb_blocks * clock_freq * 10**6 * seconds_per_day
 	two_exp = math.log(encrypt_per_day, 2)
 
-	print ()
+	encrypt_per_block_day = clock_freq * 10**6 * seconds_per_day
+	two_exp_block = math.log(encrypt_per_block_day, 2)
+
+	encryptions_per_block = 2**(64-N)
+
+	seconds_per_block = encryptions_per_block / float(clock_freq * 10**6)
+
+	hours_per_block = seconds_per_block / float(60*60)
+
+	print 
 	print ("################################################################################")
-	print ()
+	print 
 
-	print ("Clock frequency     : " + str(clock_freq))
-	print ("DES blocks          : " + str(nb_blocks))
-	print ("Encryptions per day : " + str(encrypt_per_day))
-	print ("Encryptions 2 exp   : " + str(two_exp))
+	print ("Clock frequency            : " + str(clock_freq))
+	print ("DES cores                  : " + str(nb_blocks))
+	print ("Encryptions per day (block): " + str(encrypt_per_block_day))
+	print ("Encryptions 2 exp          : " + str(two_exp_block))
+	print ("Encryptions per day (total): " + str(encrypt_per_day))
+	print ("Encryptions 2 exp          : " + str(two_exp))
+	print
 
-	print ()
-
+	print ("Time for core complete     : " + str(hours_per_block) + " hours for " + str(N) + " bit region")
 if __name__ == '__main__':
     main()
 
