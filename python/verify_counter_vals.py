a159802a733961630b66050eab9e838e227aad4c
stef-dhaeseleer <stefdhaeseleer@gmail.com>
2018-12-04 16:06:47 +0100

    Finished verify counter function

--- python/verify_counter_vals.py
+++ python/verify_counter_vals.py
@@ -9,11 +9,15 @@ def main():
 
 	bias_expected = 2**(-13.5)      # expected bias for 8 round test
 
+    bias = abs((count/exp) - 0.5)
 
-    bias = (count/exp) - 0.5
+    diff = abs(bias - bias_expected)
 
 	print 
 	print ("################################################################################")
+    print ("Expected bias   : " + str(bias_expected))
+    print ("Calculated bias : " + str(bias))
+    print ("Difference      : " + str(diff))
 	print 
 
 	print ()
