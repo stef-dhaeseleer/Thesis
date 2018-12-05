997ebcaced9519bb6f15d8b71f8fbcde39a323fd
stef-dhaeseleer <stefdhaeseleer@gmail.com>
2018-12-05 09:12:13 +0100

    Working on counter verification file

--- python/verify_counter_vals.py
+++ python/verify_counter_vals.py
@@ -1,27 +1,55 @@
 import math
 
-def main():
+def one_verify():
 
-    N = 27              # Number of region bits
-    
-	count = 200	        # Counter value
-	exp = 2**(64-N)	    # Nb of encryptions = 2**(64-N)
+    N = 27                      # Number of region bits
 
-	bias_expected = 2**(-13.5)      # expected bias for 8 round test
+    count = 0x0000000fff996560 # Counter value
+    exp = 2**(64-N)            # Nb of encryptions = 2**(64-N)
+
+    bias_expected = 2**(-13.5)      # expected bias for 8 round test
 
     bias = abs((count/exp) - 0.5)
 
     diff = abs(bias - bias_expected)
 
-	print 
-	print ("################################################################################")
+    print ()
+    print ("################################################################################")
     print ("Expected bias   : " + str(bias_expected))
     print ("Calculated bias : " + str(bias))
     print ("Difference      : " + str(diff))
-	print 
+    print ()
 
-	print ()
-	print
+    print ()
+    print ()
+
+def all_verify():
+
+    file_input = open("testfiles/report.txt", "r")
+
+    N = 27                      # Number of region bits
+    exp = 2**(64-N)            # Nb of encryptions = 2**(64-N)
+    bias_expected = 2**(-13.5)      # expected bias for 8 round test
+
+    count = 0x0000000fff996560 # Counter value
+
+    bias = abs((count/exp) - 0.5)
+
+    diff = abs(bias - bias_expected)
+
+    print ()
+    print ("################################################################################")
+    print ("Expected bias   : " + str(bias_expected))
+    print ("Calculated bias : " + str(bias))
+    print ("Difference      : " + str(diff))
+    print ()
+
+    print ()
+    print ()
+
+def main():
+
+    one_verify()
 
 if __name__ == '__main__':
     main()
