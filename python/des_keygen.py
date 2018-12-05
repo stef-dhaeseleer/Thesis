49a7e6dc39956ce8c124def68ec3dd039e74d458
stef-dhaeseleer <stefdhaeseleer@gmail.com>
2018-12-05 20:53:40 +0100

    File for generating the keys is ready

--- python/des_keygen.py
+++ python/des_keygen.py
@@ -4,6 +4,8 @@
 # https://medium.com/@urwithajit9/how-to-teach-des-using-python-the-easy-way-part-2-round-function-f-285dd3aef34d
 # https://medium.com/@urwithajit9/how-to-teach-des-using-python-the-easy-way-part-3-des-encryption-4394a935effc
 
+import random
+
 PC1_values = [57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4]
 PC2_values = [14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2, 41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32]
 round_shifts = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
@@ -55,8 +57,8 @@ def generate_keys(master_key):
 
 	return round_keys
 
-def main():
-    
+def gen_single_key_file():
+
     #master_key = "0001001100110100010101110111100110011011101111001101111111110001"
     master_key = "0000000100010011101110010111000011111101001101001111001011001110"
     round_keys = generate_keys(master_key)
@@ -78,6 +80,28 @@ def main():
     print ("File generated!")
 
     print
+
+def gen_32_key_file():
+
+    file = open("testfiles/32_key_file.txt", "w")
+
+    for i in range(0, 32):
+        master_key = "{0:b}".format(random.getrandbits(128))
+        round_keys = generate_keys(master_key)
+     
+        file.write(round_keys[0] + round_keys[1] + round_keys[2] + round_keys[3] + round_keys[4] + round_keys[5] + round_keys[6] + round_keys[7] + round_keys[8] + round_keys[9] + round_keys[10] + round_keys[11] + round_keys[12] + round_keys[13] + round_keys[14] + round_keys[15]+ "\n") 
+
+    file.close()
+
+    print
+
+    print ("File generated!")
+
+    print    
+
+def main():
+    
+    gen_32_key_file()
 
 if __name__ == '__main__':
     main()
