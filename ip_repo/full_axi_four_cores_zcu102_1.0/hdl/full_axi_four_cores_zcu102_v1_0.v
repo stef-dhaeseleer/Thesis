38660d95bba569615b8aa61202fd0d5d0f6dfdeb
Stef D'haeseleer <r0583050@pc-klas5-15.esat.kuleuven.be>
2019-03-05 17:24:56 +0100

    New AXI peripheral

---
+++ ip_repo/full_axi_four_cores_zcu102_1.0/hdl/full_axi_four_cores_zcu102_v1_0.v
@@ -0,0 +1,115 @@
+
+`timescale 1 ns / 1 ps
+
+	module full_axi_four_cores_zcu102_v1_0 #
+	(
+            // Users to add parameters here
+    
+            // User parameters ends
+            // Do not modify the parameters beyond this line
+    
+    
+            // Parameters of Axi Slave Bus Interface S00_AXI
+            parameter integer C_S00_AXI_DATA_WIDTH    = 32,
+            parameter integer C_S00_AXI_ADDR_WIDTH    = 5
+        )
+        (
+            // Users to add ports here
+    
+            input wire des_clk,
+    
+            // User ports ends
+            // Do not modify the ports beyond this line
+    
+    
+            // Ports of Axi Slave Bus Interface S00_AXI
+            input wire  s00_axi_aclk,
+            input wire  s00_axi_aresetn,
+            input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_awaddr,
+            input wire [2 : 0] s00_axi_awprot,
+            input wire  s00_axi_awvalid,
+            output wire  s00_axi_awready,
+            input wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_wdata,
+            input wire [(C_S00_AXI_DATA_WIDTH/8)-1 : 0] s00_axi_wstrb,
+            input wire  s00_axi_wvalid,
+            output wire  s00_axi_wready,
+            output wire [1 : 0] s00_axi_bresp,
+            output wire  s00_axi_bvalid,
+            input wire  s00_axi_bready,
+            input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_araddr,
+            input wire [2 : 0] s00_axi_arprot,
+            input wire  s00_axi_arvalid,
+            output wire  s00_axi_arready,
+            output wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_rdata,
+            output wire [1 : 0] s00_axi_rresp,
+            output wire  s00_axi_rvalid,
+            input wire  s00_axi_rready
+        );
+    
+        // ********************
+    
+        wire [63:0] des_counter;
+        wire [31:0] cmd_data;
+        wire [31:0] des_data_upper;
+        wire [31:0] des_data_lower;
+    
+         wire cmd_data_valid;
+        wire cmd_data_read;
+        wire des_done;
+    
+        // ********************
+        
+// Instantiation of Axi Bus Interface S00_AXI
+	full_axi_four_cores_zcu102_v1_0_S00_AXI # ( 
+		.C_S_AXI_DATA_WIDTH(C_S00_AXI_DATA_WIDTH),
+		.C_S_AXI_ADDR_WIDTH(C_S00_AXI_ADDR_WIDTH)
+	) full_axi_four_cores_zcu102_v1_0_S00_AXI_inst (
+		.S_AXI_ACLK(s00_axi_aclk),
+		.S_AXI_ARESETN(s00_axi_aresetn),
+		.S_AXI_AWADDR(s00_axi_awaddr),
+		.S_AXI_AWPROT(s00_axi_awprot),
+		.S_AXI_AWVALID(s00_axi_awvalid),
+		.S_AXI_AWREADY(s00_axi_awready),
+		.S_AXI_WDATA(s00_axi_wdata),
+		.S_AXI_WSTRB(s00_axi_wstrb),
+		.S_AXI_WVALID(s00_axi_wvalid),
+		.S_AXI_WREADY(s00_axi_wready),
+		.S_AXI_BRESP(s00_axi_bresp),
+		.S_AXI_BVALID(s00_axi_bvalid),
+		.S_AXI_BREADY(s00_axi_bready),
+		.S_AXI_ARADDR(s00_axi_araddr),
+		.S_AXI_ARPROT(s00_axi_arprot),
+		.S_AXI_ARVALID(s00_axi_arvalid),
+		.S_AXI_ARREADY(s00_axi_arready),
+		.S_AXI_RDATA(s00_axi_rdata),
+		.S_AXI_RRESP(s00_axi_rresp),
+		.S_AXI_RVALID(s00_axi_rvalid),
+		.S_AXI_RREADY(s00_axi_rready),
+        // **************************************
+        .CMD_DATA(cmd_data),
+        .CMD_DATA_VALID(cmd_data_valid),
+        .CMD_DATA_READ(cmd_data_read),
+        .DES_DONE(des_done),
+        .DES_COUNTER(des_counter),
+        .DATA_UPPER(des_data_upper),
+        .DATA_LOWER(des_data_lower)
+    );
+    
+    // Add user logic here
+
+    des_block_wrapper des_block_wrapper(
+        .clk (des_clk),
+        .rst_n (s00_axi_aresetn),
+        .cmd (cmd_data),
+        .cmd_valid (cmd_data_valid),
+        // **********************************
+        .data_upper (des_data_upper),
+        .data_lower (des_data_lower),
+        .cmd_read (cmd_data_read),
+        .done (des_done),
+        .counter (des_counter)
+        );
+
+    // User logic ends
+
+    endmodule
