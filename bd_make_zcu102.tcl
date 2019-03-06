e613c59a8dd40d47cb26d80b9b6dfecd1b86babb
Stef D'haeseleer <r0583050@pc-klas5-14.esat.kuleuven.be>
2019-03-06 15:51:00 +0100

    tcl

--- bd_make_zcu102.tcl
+++ bd_make_zcu102.tcl
@@ -1,6 +1,9 @@
 set name "full_axi_four_cores_zcu102_" 
 set type "full_axi_four_cores_zcu102" 
 
+set pre "/"
+set post "/S00_AXI"
+
 startgroup
 create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.0 zynq_ultra_ps_e_0
 apply_bd_automation -rule xilinx.com:bd_rule:zynq_ultra_ps_e -config {apply_board_preset "1" }  [get_bd_cells zynq_ultra_ps_e_0]
@@ -9,11 +12,12 @@ endgroup
 
 for {set count 0} {$count < 64} {incr count} {
     set a $name$count
+    set b [format %s%s%s $pre $a $post]
     startgroup
     create_bd_cell -type ip -vlnv user.org:user:$type:1.0 $a
     apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM0_FPD" intc_ip "New AXI Interconnect" Clk_xbar "Auto" Clk_master "Auto" Clk_slave "Auto" }  [get_bd_intf_pins $a/S00_AXI]
     apply_bd_automation -rule xilinx.com:bd_rule:clkrst -config {Clk "/zynq_ultra_ps_e_0/pl_clk1 (214 MHz)" }  [get_bd_pins $a/des_clk]
-    apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Slave "/"$a"/S00_AXI" intc_ip "/ps8_0_axi_periph" Clk_xbar "Auto" Clk_master "Auto" Clk_slave "Auto" }  [get_bd_intf_pins zynq_ultra_ps_e_0/M_AXI_HPM1_FPD]
+    apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Slave $b intc_ip "/ps8_0_axi_periph" Clk_xbar "Auto" Clk_master "Auto" Clk_slave "Auto" }  [get_bd_intf_pins zynq_ultra_ps_e_0/M_AXI_HPM1_FPD]
     endgroup
 }
 
