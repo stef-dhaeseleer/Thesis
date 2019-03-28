3d6cfc96cdae968f2f6edc111974dedc69b3d1f3
Stef Dhaeseleer <stefdhaeseleer@gmail.com>
2019-03-28 11:03:56 +0100

    Reordering files and changing python to new key

--- python/verify_counter_vals.py
+++ python/verify_counter_vals.py
@@ -57,18 +57,18 @@ def get_mean_std():
 def all_verify():
 
     file = open("counter_results/results.txt", "r")
-    file_key = open("counter_results/result_keys.txt", "r")
+#    file_key = open("counter_results/result_keys.txt", "r")
 
-    keys  = []
+#    keys  = []
 
-    for line in file_key:
-        keys.append(line)    
+#    for line in file_key:
+#        keys.append(line)    
 
     file_res = open("counter_results/bias_results.txt", "a")
 
-    N = 32                     # Number of region bits
+    N = 13                     # Number of region bits
     exp = 2**(64-N)            # Nb of encryptions = 2**(64-N)
-    bias_expected = 2**(-13.5)      # expected bias for 8 round test
+#    bias_expected = 2**(-13.5)      # expected bias for 8 round test
 
     total_count = 0
     total_lines = 0
@@ -78,27 +78,27 @@ def all_verify():
     i = 0
 
     for line in file:
-        region = line.split()[0]
-        count = line.split()[2]
+        poly = line.split()[0]
+        count = line.split()[4]
         count = int(count, 16)
         bias = abs((count/float(exp)) - 0.5)
-        diff = abs(bias - bias_expected)
+#        diff = abs(bias - bias_expected)
 
         total_count += count
         total_lines += 1
 
         print 
         print ("################################################################################")
-        print ("Expected bias   : " + str(bias_expected))
+#        print ("Expected bias   : " + str(bias_expected))
         print ("Calculated bias : " + str(bias))
-        print ("Difference      : " + str(diff))
+#       print ("Difference      : " + str(diff))
 
-        file_res.write("Region          : " + region + "\n")
-        file_res.write("Expected bias   : " + str(bias_expected) + "\n")
+        file_res.write("Polynomial      : " + poly + "\n")
+#        file_res.write("Expected bias   : " + str(bias_expected) + "\n")
         file_res.write("Calculated bias : " + str(bias) + "\n")
-        file_res.write("Difference      : " + str(diff) + "\n")
+#        file_res.write("Difference      : " + str(diff) + "\n")
 
-        file_res.write("Key             : " + keys[i%32] + "\n")   # Remainder for if multiple runs with the same keys
+#        file_res.write("Key             : " + keys[i%32] + "\n")   # Remainder for if multiple runs with the same keys
         file_res.write("\n")
 
         i += 1
@@ -106,18 +106,19 @@ def all_verify():
     # Now process the complete result as well
 
     bias = abs((total_count/float(exp*total_lines)) - 0.5)
-    diff = abs(bias - bias_expected)
+#    diff = abs(bias - bias_expected)
 
     print 
     print ("################################################################################")
     print ("FINAL RESULT OVER ALL COUNTERS")
-    print ("Expected bias   : " + str(bias_expected))
+#    print ("Expected bias   : " + str(bias_expected))
     print ("Calculated bias : " + str(bias))
-    print ("Difference      : " + str(diff))
+#    print ("Difference      : " + str(diff))
+    print
 
 def main():
 
-    get_mean_std()
+    all_verify()
 
 if __name__ == '__main__':
     main()
